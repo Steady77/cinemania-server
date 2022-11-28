ccabf69ce178c4f145beef01379735ababce51d1
Surov Denis <90111915+Steady77@users.noreply.github.com>
2022-11-28 18:28:44 +0300

    feat: add endpoint to update user profile

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import pool from '../db.js';
 import { authorize } from '../middleware/authorize.js';
+import { genSalt, hash } from 'bcrypt';
 
 const router = express.Router();
 
@@ -20,6 +21,38 @@ router.get('/profile', authorize, async (req, res) => {
       isAdmin: user.rows[0].is_admin,
       avatar: user.rows[0].avatar,
     });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+router.put('/profile', authorize, async (req, res) => {
+  try {
+    const { id } = req.user;
+    const { email, password } = req.body;
+
+    const user = await pool.query('SELECT * FROM users WHERE id = $1', [id]);
+    const sameUser = await pool.query('SELECT * FROM users WHERE email = $1', [email]);
+
+    const isUser = user.rows.length > 0;
+    const isSameUser = sameUser.rows.length > 0;
+
+    if (isSameUser && id !== sameUser.rows[0].id) {
+      return res.status(401).json({ message: 'Почта с таким именем уже занята' });
+    }
+
+    if (isUser) {
+      if (password) {
+        const salt = await genSalt(10);
+        const hachedPassword = await hash(password, salt);
+
+        await pool.query('UPDATE users SET password = $1 WHERE id = $2', [hachedPassword, id]);
+      }
+
+      await pool.query('UPDATE users SET email = $1 WHERE id = $2', [email, id]);
+    }
+
+    res.status(200).json({ message: 'Профиль обновлен' });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
