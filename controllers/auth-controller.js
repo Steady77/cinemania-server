d287021aaed6489355c6f3153c46700b6e6a8b92
Surov Denis <90111915+Steady77@users.noreply.github.com>
2022-12-19 18:28:18 +0300

    chore: change expiresIn for jwt token

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -3,67 +3,67 @@ import ApiError from '../exeptions/api-error.js';
 import AuthService from '../services/auth-service.js';
 
 class AuthController {
-  async register(req, res, next) {
-    try {
-      const errors = validationResult(req);
-      if (!errors.isEmpty()) {
-        return next(ApiError.BadRequest('Ошибка при валидации', errors.array()));
-      }
+	async register(req, res, next) {
+		try {
+			const errors = validationResult(req);
+			if (!errors.isEmpty()) {
+				return next(ApiError.BadRequest('Ошибка при валидации', errors.array()));
+			}
 
-      const { email, password } = req.body;
+			const { email, password } = req.body;
 
-      const user = await AuthService.register(email, password);
+			const user = await AuthService.register(email, password);
 
-      res.cookie('refresh_token', user.refreshToken, {
-        maxAge: 7 * 24 * 60 * 60 * 1000,
-        httpOnly: true,
-      });
+			res.cookie('refresh_token', user.refreshToken, {
+				maxAge: 30 * 24 * 60 * 60 * 1000,
+				httpOnly: true,
+			});
 
-      return res.status(200).json(user);
-    } catch (error) {
-      next(error);
-    }
-  }
+			return res.status(200).json(user);
+		} catch (error) {
+			next(error);
+		}
+	}
 
-  async login(req, res, next) {
-    const { email, password } = req.body;
+	async login(req, res, next) {
+		const { email, password } = req.body;
 
-    try {
-      const user = await AuthService.login(email, password);
+		try {
+			const user = await AuthService.login(email, password);
 
-      res.cookie('refresh_token', user.refreshToken, {
-        httpOnly: true,
-        sameSite: 'none',
-      });
+			res.cookie('refresh_token', user.refreshToken, {
+				httpOnly: true,
+				sameSite: 'none',
+			});
 
-      return res.status(200).json(user);
-    } catch (error) {
-      next(error);
-    }
-  }
+			return res.status(200).json(user);
+		} catch (error) {
+			next(error);
+		}
+	}
 
-  async getAccessToken(req, res, next) {
-    try {
-      const { refreshToken } = req.body;
+	async getAccessToken(req, res, next) {
+		try {
+			const { refreshToken } = req.body;
 
-      const user = await AuthService.getAccessToken(refreshToken);
+			const user = await AuthService.getAccessToken(refreshToken);
 
-      res.cookie('refresh_token', user.refreshToken, { httpOnly: true, sameSite: 'none' });
+			res.cookie('refresh_token', user.refreshToken, { httpOnly: true, sameSite: 'none' });
 
-      return res.status(200).json(user);
-    } catch (error) {
-      next(error);
-    }
-  }
+			return res.status(200).json(user);
+		} catch (error) {
+			next(error);
+		}
+	}
 
-  async logout(res, next) {
-    try {
-      res.clearCookie('refresh_token');
-      return res.status(200).json({ message: 'Токен удален' });
-    } catch (error) {
-      next(error);
-    }
-  }
+	async logout(res, next) {
+		try {
+			res.clearCookie('refresh_token');
+			return res.status(200).json({ message: 'Токен удален' });
+		} catch (error) {
+			next(error);
+		}
+	}
 }
 
 export default new AuthController();
--- utils/jwt-helpers.js
+++ utils/jwt-helpers.js
@@ -1,15 +1,15 @@
 import jwt from 'jsonwebtoken';
 
 export function jwtTokens(id) {
-  const data = {
-    id: id,
-  };
+	const data = {
+		id: id,
+	};
 
-  const accessToken = jwt.sign(data, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
-  const refreshToken = jwt.sign(data, process.env.REFRESH_TOKEN_SECRET, { expiresIn: '7d' });
+	const accessToken = jwt.sign(data, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '7d' });
+	const refreshToken = jwt.sign(data, process.env.REFRESH_TOKEN_SECRET, { expiresIn: '30d' });
 
-  return {
-    accessToken,
-    refreshToken,
-  };
+	return {
+		accessToken,
+		refreshToken,
+	};
 }
