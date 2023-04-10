ad6854b4367a9c35f3c92c475e29cdf57c04f3cc
Surov Denis <90111915+Steady77@users.noreply.github.com>
2023-04-10 19:48:18 +0300

    fix: correct removal user from tables

--- services/admin-service.js
+++ services/admin-service.js
@@ -1,26 +1,27 @@
 import pool from '../db.js';
 
 class AdminService {
-  async getAllUsers(keyword) {
-    let users;
+	async getAllUsers(keyword) {
+		let users;
 
-    if (keyword) {
-      users = await pool.query(
-        'SELECT id, email, created_at, is_admin FROM users WHERE email LIKE $1',
-        ['%' + keyword + '%'],
-      );
-    }
+		if (keyword) {
+			users = await pool.query(
+				'SELECT id, email, created_at, is_admin FROM users WHERE email LIKE $1',
+				['%' + keyword + '%'],
+			);
+		}
 
-    if (!keyword) {
-      users = await pool.query('SELECT id, email, created_at, is_admin FROM users');
-    }
+		if (!keyword) {
+			users = await pool.query('SELECT id, email, created_at, is_admin FROM users');
+		}
 
-    return users.rows;
-  }
+		return users.rows;
+	}
 
-  async deleteUser(id) {
-    await pool.query('DELETE FROM users WHERE id = $1', [id]);
-  }
+	async deleteUser(id) {
+		await pool.query('DELETE FROM favorites WHERE user_id = $1', [id]);
+		await pool.query('DELETE FROM users WHERE id = $1', [id]);
+	}
 }
 
 export default new AdminService();
